d8da9e6e4507474d40a3dc0873eb1441c0bc6757
bakunovgerman <german.bakunov@gmail.com>
2021-04-20 16:14:46 +0700

    сверстано активити деталей заказа

--- app/src/main/java/com/example/socrm/DetailOrderActivity.java
+++ app/src/main/java/com/example/socrm/DetailOrderActivity.java
@@ -8,11 +8,25 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 import android.widget.TextView;
 
 import com.example.socrm.data.Order;
+import com.google.android.material.textfield.TextInputLayout;
 
 public class DetailOrderActivity extends AppCompatActivity {
+
+    private AutoCompleteTextView deliveryItem;
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        String[] delivery_array = getResources().getStringArray(R.array.delivery_input);
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(getApplicationContext(), R.layout.dropdown_item, delivery_array);
+        deliveryItem = findViewById(R.id.deliveryItem);
+        deliveryItem.setAdapter(arrayAdapter);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +37,9 @@ public class DetailOrderActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setDisplayShowHomeEnabled(true);
+
+
+
 
         Bundle arguments = getIntent().getExtras();
         toolbar.setNavigationOnClickListener(new View.OnClickListener() {
